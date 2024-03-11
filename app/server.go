293c83c53475d4095e39765fbc6b747acfe1b625
jals413 <jalsthakor413@gmail.com>
2024-03-11 15:43:22 +0530

    respond with 404

--- app/server.go
+++ app/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,17 +14,37 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go requestHandler(conn)
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+	}
+}
+
+func requestHandler(conn net.Conn) {
+
+	defer conn.Close()
+	statusOK := "HTTP/1.1 200 OK\r\n\r\n"
+	status404 := "HTTP/1.1 404 Not Found\r\n\r\n"
+	scanner := bufio.NewScanner(conn)
+
+	if !scanner.Scan() {
+		fmt.Printf("[Error while reading data: %s]\n", conn.RemoteAddr())
 		os.Exit(1)
 	}
-	defer conn.Close()
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection", err.Error())
-		os.Exit(1)
+
+	request := scanner.Text()
+	splittedReq := strings.Split(request, " ")
+
+	switch {
+	case splittedReq[1] == "/":
+		conn.Write([]byte(statusOK))
+	default:
+		conn.Write([]byte(status404))
 	}
 
 }
