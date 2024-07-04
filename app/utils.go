69fbf88e815f97baf5e3379ab903623394816542
jals413 <jalsthakor413@gmail.com>
2024-07-04 15:47:12 +0530

    codecrafters submit [skip ci]

--- app/utils.go
+++ app/utils.go
@@ -1,41 +1,38 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"os"
 	"strings"
 )
 
 func requestHandler(conn net.Conn) {
 
 	defer conn.Close()
+
 	statusOK := "HTTP/1.1 200 OK\r\n\r\n"
 	status404 := "HTTP/1.1 404 Not Found\r\n\r\n"
-	scanner := bufio.NewScanner(conn)
 
-	if !scanner.Scan() {
-		fmt.Printf("[Error while reading data: %s]\n", conn.RemoteAddr())
-		os.Exit(1)
-	}
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
 
-	request := scanner.Text()
-	splittedReq := strings.Split(request, " ")
+	splitedHeader := strings.Split(string(buffer), "\r\n")
+	splitedRequestLine := strings.Split(splitedHeader[0], " ")
+	splitedUserAgent := strings.Split(splitedHeader[2], " ")
 
-	if len(splittedReq) != 3 {
-		fmt.Println("Invalid request")
-		os.Exit(1)
-	}
-	if splittedReq[1] == "/" {
+	if splitedRequestLine[1] == "/" {
 		conn.Write([]byte(statusOK))
 		return
-	}
-	if !strings.HasPrefix(splittedReq[1], "/echo/") {
+	} else if strings.Split(splitedRequestLine[1], "/")[1] == "echo" {
+		param := strings.Split(splitedRequestLine[1], "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(param), param)))
+
+	} else if strings.Split(splitedRequestLine[1], "/")[1] == "user-agent" {
+		param := strings.Split(splitedUserAgent[1], ":")[0]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(param), param)))
+	} else {
 		conn.Write([]byte(status404))
 		return
 	}
-	param := strings.Replace(splittedReq[1], "/echo/", "", 1)
-	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(param), param)))
 
 }
